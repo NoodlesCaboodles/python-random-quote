1f761009a4fb209168e6395ec52658748d3a6fd9
Rahman <armustap@gmail.com>
2020-08-08 21:27:08 -0700

    Modified random quote bot is working

--- get-quote.py
+++ get-quote.py
@@ -10,7 +10,10 @@ def primary():
   
   last = 13
   rnd = random.randint(0, last)
+  rnd2 = random.randint(0, last)
+
   print(quotes[rnd])
+  print(quotes[rnd2])
 
 if __name__== "__main__":
   primary()
